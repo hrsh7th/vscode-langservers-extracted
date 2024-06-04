601969c8f6a3e924bc8dbf4898ff1ffc4d6e1963
David Prokopec <41482710+davidprokopec@users.noreply.github.com>
2024-06-04 11:18:29 +0200

    improve vscode-eslint build script to lock to latest stable release

--- script/build-vscode-eslint.sh
+++ script/build-vscode-eslint.sh
@@ -2,19 +2,22 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+# Latest stable release for vscode-eslint is 2.4.4
+# https://github.com/microsoft/vscode-eslint/tags
+RELEASE_TAG="release/2.4.4"
+
 # prepare
 mkdir -p $DIR/../tmp
 mkdir -p $DIR/../dist
 
 # clone
 cd $DIR/../tmp
-git clone --depth=1 git@github.com:Microsoft/vscode-eslint vscode-eslint
+git clone git@github.com:Microsoft/vscode-eslint vscode-eslint
 
 # pull
 cd $DIR/../tmp/vscode-eslint
 git clean -fd
-git checkout .
-git pull --rebase
+git checkout tags/$RELEASE_TAG
 
 # npm install
 cd $DIR/../tmp/vscode-eslint
